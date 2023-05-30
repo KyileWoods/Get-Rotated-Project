e1c96cfa7a07014ccb0e3349706e4704b08747c7
Kyile Woods <kyile.woods@connect.qut.edu.au>
2023-05-30 15:56:05 +1000

    Made a demand on myself. Cruel Master.

---
+++ Debug/ui/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ui/user_interface.c 
+
+C_DEPS += \
+./ui/user_interface.d 
+
+OBJS += \
+./ui/user_interface.obj 
+
+OBJS__QUOTED += \
+"ui\user_interface.obj" 
+
+C_DEPS__QUOTED += \
+"ui\user_interface.d" 
+
+C_SRCS__QUOTED += \
+"../ui/user_interface.c" 
+
+
